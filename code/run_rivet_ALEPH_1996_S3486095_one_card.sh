c01199587810dd1dc0795bad66653cefb0380760
Ali Alkadhim <aa18dg@fsu.edu>
2024-01-18 14:55:49 -0600

    notebook chi2

--- code/run_rivet_ALEPH_1996_S3486095_one_card.sh
+++ code/run_rivet_ALEPH_1996_S3486095_one_card.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./main42 ALEPH_1996_S3486095_Cards/ALEPH_1996_S3486095_card_0.cmnd ALEPH_1996_S3486095_card_0.fifo &
-rivet -o rivet_histograms/ALEPH_1996_S3486095_card_0.yoda -a ALEPH_1996_S3486095 ALEPH_1996_S3486095_card_0.fifo
+rivet -o rivet_histograms/simulation/ALEPH_1996_S3486095_card_0.yoda -a ALEPH_1996_S3486095 ALEPH_1996_S3486095_card_0.fifo
 
 rm ALEPH_1996_S3486095_card_0.fifo
+yoda2root rivet_histograms/simulation/ALEPH_1996_S3486095_card_0.yoda rivet_histograms/simulation/ALEPH_1996_S3486095_card_0.root
