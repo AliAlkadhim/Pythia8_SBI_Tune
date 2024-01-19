e637bd1eb1e1688fc539614a77aaab7c629a4580
Ali Alkadhim <aa18dg@fsu.edu>
2024-01-19 12:05:12 -0600

    yoda histograms

--- code/docker_run.sh
+++ code/docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#docker run -v $PWD:$PWD -w $PWD -p 8889:8889 -it pythia8/tutorials:hsf23
+#docker run -v $PWD:$PWD -w $PWD -p 8888:8888 -it pythia8/tutorials:hsf23
 # then jupyter-lab --ip 0.0.0.0 --port 8888 --allow-root &
 
 #docker run -v $PWD:$PWD -w $PWD -p 8889:8889 -it hepstore/rivet-pythia
