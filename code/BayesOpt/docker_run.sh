53342a2a7dfe4d2bdd6a153991948542f81c8264
Ali Alkadhim <aa18dg@fsu.edu>
2024-05-31 14:59:53 -0500

    save and reload experiment

--- code/BayesOpt/docker_run.sh
+++ code/BayesOpt/docker_run.sh
@@ -3,4 +3,4 @@
 # then jupyter-lab --ip 0.0.0.0 --port 8888 --allow-root &
 
 #docker run -v $PWD:$PWD -w $PWD -p 8889:8889 -it hepstore/rivet-pythia
-docker run -v $PWD:$PWD -w $PWD -p 8889:8889 -it alialkadhim/pythia_sbi_tune:latest
+docker run -v $PWD:$PWD -v /media/ali/DATA/TEMP:/media/ali/DATA/TEMP -w $PWD -p 8889:8889 -it alialkadhim/pythia_sbi_tune:latest
